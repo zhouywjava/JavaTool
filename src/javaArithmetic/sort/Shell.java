595f42eec66fb3cb43d4a74e028db13d5e9f4295
zhouywjava <67976661@qq.com>
2018-02-27 13:24:47 +0800

    study arithmetic shell and sortCompare

--- src/javaArithmetic/sort/Shell.java
+++ src/javaArithmetic/sort/Shell.java
@@ -8,11 +8,32 @@ import javaArithmetic.sort.base.BaseSort;
  * 2.插入排序一般情况是。假设长度为n,倒序为m 的交换m次，比较m次，同一次比较中的倒序有可能会多引起一次比较（只比较不交换）大概是1~n-2次，正序的次数n-m ,总的就是 n-m + m + m + (1~n-2) = n+m+1 ~ n+m+n-2  其中 设一般情况m = n/2 所以就是
  *  3*n/2 ~ 5*n /2
  * 3.希尔排序解决的问题就是，如果逆序越少，则效率越高。所以采取的优化策略是先大跨步的进行粗略排序，后减少步长，当步长减少到1后就是一般的插入排序，这时序列已经相对有序了。
+ * 4.步长如何确定没有固定的说法。每轮的步长减少可以不是1。
  * Created by zhouyiwei on 2018/2/23.
  */
 public class Shell implements BaseSort {
     @Override
     public void sort(Comparable[] a) {
+        int n = a.length;
+        int h = 1 ;
+        if(h< n / 3){
+            h = 3*h +1;
+        }
+       while(h>=1){
+        for(int i = h;i< n ;i ++){
+            //如果j比j-h大，那么就结束内部循环，因为j-h-h已经比j-h小了，所以肯定比j小，不需要继续比较
+            for(int j = i;j>=h && BaseSort.less(a[j],a[j-h]);j -= h){
+                BaseSort.exch(a,j,j-h);
+            }
+        }
+        h = h/3;
+       }
+    }
 
+    public static void main(String[] args) {
+        Integer[] a = {2,4,1,5,6,7,8,0,3,9};
+        BaseSort shell = new Shell();
+        shell.sort(a);
+        BaseSort.show(a);
     }
 }
--- src/javaArithmetic/sort/SortCompare.java
+++ src/javaArithmetic/sort/SortCompare.java
@@ -1,7 +1,12 @@
 package javaArithmetic.sort;
 
+import com.sun.javafx.binding.StringFormatter;
 import javaArithmetic.sort.base.BaseSort;
+import javaArithmetic.util.StdRandom;
 import javaArithmetic.util.Stopwatch;
+
+import java.text.Format;
+import java.util.Random;
 
 /**
  * 比较排序效率的方法
@@ -20,8 +25,8 @@ public class SortCompare {
 
        Stopwatch stopwatch = new Stopwatch();
        BaseSort sort = null;
-       if(alg.equals("Insertion")) sort = new InsertSort();
-       if(alg.equals("Selection")) sort = new Selection();
+       if(alg.equals("insertsort")) sort = new InsertSort();
+       if(alg.equals("selection")) sort = new Selection();
        if(alg.equals("shell")) sort = new Shell();
        sort.sort(a);
       return stopwatch.elapsedTime();
@@ -31,15 +36,34 @@ public class SortCompare {
       * 描述: 模拟随机输入<br>
       * 作者: zyw<br>
       * @param  alg 算法关键字
-      * @param N 创建N个数组
-      * @param T 每个数组T个元素
+      * @param N 创建长度为N的数组
+      * @param T 重复测试T次
       * @result
       */
    public static double timeRandomInput(String alg,int N,int T){
-       double total = 0.0d;
+       double totalTime = 0d;
+       Double[] arr = new Double[N];
+        for(int i = 0 ; i<T;i++){
+            for(int j = 0 ;j < N;j++) {
+                arr[j] = StdRandom.uniform();
+            }
+            totalTime += time(alg,arr);
+        }
+       return totalTime;
+   }
 
-        Integer i = new Integer(N);
-       return total;
+    public static void main(String[] args) {
+        String alg1 = args[0];
+        String alg2 = args[1];
+        String alg3 = args[2];
+        int N = Integer.valueOf(args[3]);
+        int T = Integer.valueOf(args[4]);
+        double t1 = timeRandomInput(alg1,N,T);
+        double t2 = timeRandomInput(alg2,N,T);
+        double t3 = timeRandomInput(alg3,N,T);
+        System.out.println(String.format(" For %d length integer random array,test %d times, %s is %n %.1f times faster than %s %n",N,T,alg2,t1/t2,alg1));
+        System.out.println(String.format(" For %d length integer random array,test %d times, %s is %n %.1f times faster than %s %n",N,T,alg3,t2/t3,alg2));
+        System.out.println(String.format(" For %d length integer random array,test %d times, %s is %n %.1f times faster than %s %n",N,T,alg3,t1/t3,alg1));
    }
 
 }
