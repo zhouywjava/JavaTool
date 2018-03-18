a12ca28975d35705d8ae78fcba3ccf4ac5a51f79
zhouyiwei <67976661@qq.com>
2018-03-18 17:06:53 +0800

    Sempahore SynchronousQueue test

--- src/javaThread/synchronizer/SemaphoreTest.java
+++ src/javaThread/synchronizer/SemaphoreTest.java
@@ -1,9 +1,49 @@
 package javaThread.synchronizer;
 
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
 /**
- * @Description: 信号量
+ * @Description: 信号量 Semaphore经常用于限制获取某种资源的线程数量
  * @Author: zyw
  * @Date: 2018/3/9
  */
 public class SemaphoreTest {
+
+    //假设有40个请求
+    private static final int COUNT = 40;
+    private static final ExecutorService executor = Executors.newFixedThreadPool(COUNT);
+    //假设只有15个数据库连接
+    private static final Semaphore semaphore = new Semaphore(15);
+    private static final Random random = new Random(47);
+
+    public static void main(String[] args) {
+        Runnable r = ()->{
+            try {
+                if(semaphore.availablePermits() == 0) {
+                    System.out.println(Thread.currentThread().getName() + "没有获得执行机会,进行等待");
+                }
+
+                //申请许可证
+                semaphore.acquire();
+                System.out.println(Thread.currentThread().getName() + "获得执行机会");
+                Thread.sleep(random.nextInt(3000));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }finally{
+                System.out.println(Thread.currentThread().getName() + "执行结束,释放许可证");
+                //归还许可证,不归还的话无法接收新的线程并执行
+                semaphore.release();
+            }
+
+        };
+
+        for(int i = 0;i<COUNT;i++){
+            executor.submit(r);
+        }
+    }
+
+
 }
--- src/javaThread/synchronizer/SynchronousQueueTest.java
+++ src/javaThread/synchronizer/SynchronousQueueTest.java
@@ -1,9 +1,54 @@
 package javaThread.synchronizer;
 
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.SynchronousQueue;
+
 /**
  * @Description:允许一个线程把对象交给另一个线程
  * @Author: zyw
  * @Date: 2018/3/9
  */
 public class SynchronousQueueTest {
+
+    private static final SynchronousQueue<Integer> queue = new SynchronousQueue<>();
+    private static final Random random = new Random(47);
+    private static final ExecutorService exe = Executors.newCachedThreadPool();
+
+    public static void main(String[] args) {
+        Runnable put = ()->{
+            int object = random.nextInt(100);
+            System.out.println("开始放入元素"+object+"操作...");
+            try {
+                queue.put(object);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            //放入元素会一直阻塞到获取元素的时候，才会成功放入
+            System.out.println("成功放入元素"+object);
+        };
+
+        Runnable take = () -> {
+            Integer object = null;
+            System.out.println("开始获取元素操作...");
+
+            try {
+                object = queue.take();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("成功获取元素"+object);
+        };
+
+        for(int i = 0; i< 10;i++){
+            exe.submit(put);
+        }
+        for(int i = 0; i< 5;i++){
+            exe.submit(take);
+        }
+
+    }
+
 }
